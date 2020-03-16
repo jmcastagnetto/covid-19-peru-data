2e038ae623516e857eaa5ace8a4843cf170e16a8
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-03-16 17:46:16 -0500

    updated using https://twitter.com/Minsa_Peru/status/1239569760704253952, and added partial automation scripts

---
+++ 00-run-all.R
@@ -0,0 +1,2 @@
+source("01-add-ubigeo.R")
+source("02-build-map.R")
---
+++ 02-build-map.R
@@ -0,0 +1,7 @@
+# regenerate README.md
+rmarkdown::render(
+  input = "peru-covid-19-map.Rmd",
+  output_format = "html_document",
+  output_file = "peru-covid-19-map.html"
+)
+
