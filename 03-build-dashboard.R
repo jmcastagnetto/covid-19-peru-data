fcb9841302089996383f4d3dcf9a2330d9f38e74
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-03-21 17:04:47 -0500

    fixing dashboard build

--- 03-build-dashboard.R
+++ 03-build-dashboard.R
@@ -1,7 +1,7 @@
 # regenerate dashboard
 rmarkdown::render(
   input = "dashboard-peru-covid-19.Rmd",
-  output_format = "html_document",
+  output_format = "flexdashboard::flex_dashboard",
   output_file = "dashboard-peru-covid-19.html"
 )
 
